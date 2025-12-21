efca84955839ac91be57c171188a20a9abfe1e61
Aryosetowmn <aryosetowmnwork@gmail.com>
2025-12-22 05:54:25 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,59 @@
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  // },
+
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/MainLayout.vue'), // The base layout
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('pages/DashboardPage.vue'),
+        meta: { title: 'Dashboard' },
+      },
+      {
+        path: 'staff',
+        component: () => import('pages/StaffPage.vue'),
+        meta: { title: 'Staff' },
+      },
+      {
+        path: 'members',
+        component: () => import('pages/MemberPage.vue'),
+        meta: { title: 'Anggota Gym' },
+      }, // Add this
+      {
+        path: 'info',
+        component: () => import('pages/InfoFacilityPage.vue'),
+        meta: { title: 'Informasi & Fasilitas' },
+      },
+    ],
+  },
+
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage.vue'),
+  },
+
+  {
+    path: '/register',
+    component: () => import('pages/RegisterPage.vue'),
   },
 
   // Always leave this as last one,
   // but you can also remove it
 
-  {
-    path: '/dashboard',
-    component: () => import('pages/DashboardPage.vue'),
-  },
+  // {
+  //   path: '/dashboard',
+  //   component: () => import('pages/DashboardPage.vue'),
+  // },
 
-  {
-    path: '/staff',
-    component: () => import('pages/StaffPage.vue'),
-  },
+  // {
+  //   path: '/staff',
+  //   component: () => import('pages/StaffPage.vue'),
+  // },
 
   {
     path: '/:catchAll(.*)*',
