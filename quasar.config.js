80d0ef6908b471f72fd2bf4af3bc74454bda4136
Aryosetowmn <aryosetowmnwork@gmail.com>
2026-01-01 20:14:34 +0700

    project configuration update

--- quasar.config.js
+++ quasar.config.js
@@ -11,7 +11,7 @@ export default defineConfig((/* ctx */) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['axios'],
+    boot: ['persist', 'axios', 'google-login', 'facebook-login'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
@@ -91,7 +91,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify', 'Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
